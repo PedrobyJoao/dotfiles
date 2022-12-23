88334e3894b094e54ca17450244e48e4c0ac1164
JPexplorer <jpexplorer@pop-os.localdomain>
2022-12-23 19:40:50 -0300

    refact of settings and adding some keymaps

--- lua/configs/keybinds.lua
+++ lua/configs/keybinds.lua
@@ -1,7 +1,14 @@
 local function map(m, k, v)
-    vim.keymap.set(m, k, v, { silent = true })
+    vim.keymap.set(m, k, v, { noremap = true, silent = true })
 end
 
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
 
 -- Fix n and N. Keeping cursor in center
 map('n', 'n', 'nzz')
@@ -14,10 +21,19 @@ map('i', 'KJ', '<ESC>')
 map('i', 'JK', '<ESC>')
 
 -- Delete faster
-map('n', '<leader>d', 'dd')
+map('n', '<leader>d', 'dd') 
+
+-- Don't yank when pasting
+map("v", "p", '"_dP', opts) 
 
 -- Nohl command to CRTL + N
 map('n', '<C-N>', ':nohl<CR>')
+
+-- Indent with tab
+map("v", "<S-Tab>", "<gv")
+map("v", "<Tab>", ">gv")
+map("n", "<S-Tab>", "<<")
+map("n", "<Tab>", ">>")
 
 -- Quickly save the current buffer or all buffers
 map('n', '<leader>w', '<CMD>update<CR>')
@@ -30,9 +46,40 @@ map('n', '<C-Q>', '<CMD>q<CR>')
 map('n', '<leader>o', 'o<ESC>')
 map('n', '<leader>O', 'O<ESC>')
 
--- Move to the next/previous buffer
-map('n', '<leader>[', '<CMD>bp<CR>')
-map('n', '<leader>]', '<CMD>bn<CR>')
+-- Move line up and down in NORMAL and VISUAL modes
+-- Reference: https://vim.fandom.com/wiki/Moving_lines_up_or_down
+map('n', '<A-j>', '<CMD>move .+1<CR>')
+map('n', '<A-k>', '<CMD>move .-2<CR>')
+map('x', '<A-j>', ":move '>+1<CR>gv=gv")
+map('x', '<A-k>', ":move '<-2<CR>gv=gv")
+
+-- Use operator pending mode to visually select the whole buffer
+-- e.g. dA = delete buffer ALL, yA = copy whole buffer ALL
+map('o', 'A', ':<C-U>normal! mzggVG<CR>`z')
+map('x', 'A', ':<C-U>normal! ggVG<CR>')
+
+---------------------- Files ----------------------
+
+-- Easy access for file tree
+map('n', '<leader>e', ':Lex 30<CR>')
+
+---------------------- Windows and Buffers ----------------------
+
+-- Better window navigation
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
+-- Resize with arrows
+map("n", "<C-Up>", ":resize -2<CR>")
+map("n", "<C-Down>", ":resize +2<CR>")
+map("n", "<C-Left>", ":vertical resize -2<CR>")
+map("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Navigate buffers
+map("n", "<S-l>", ":bnext<CR>")
+map("n", "<S-h>", ":bprevious<CR>")
 
 -- Move to last buffer
 map('n', "''", '<CMD>b#<CR>')
@@ -41,14 +88,4 @@ map('n', "''", '<CMD>b#<CR>')
 map('n', '<C-\\>', '<CMD>vsplit<CR>')
 map('n', '<A-\\>', '<CMD>split<CR>')
 
--- Move line up and down in NORMAL and VISUAL modes
--- Reference: https://vim.fandom.com/wiki/Moving_lines_up_or_down
-map('n', '<C-j>', '<CMD>move .+1<CR>')
-map('n', '<C-k>', '<CMD>move .-2<CR>')
-map('x', '<C-j>', ":move '>+1<CR>gv=gv")
-map('x', '<C-k>', ":move '<-2<CR>gv=gv")
 
--- Use operator pending mode to visually select the whole buffer
--- e.g. dA = delete buffer ALL, yA = copy whole buffer ALL
-map('o', 'A', ':<C-U>normal! mzggVG<CR>`z')
-map('x', 'A', ':<C-U>normal! ggVG<CR>')
--- lua/configs/settings.lua
+++ lua/configs/settings.lua
@@ -1,82 +1,48 @@
-local g = vim.g
-local o = vim.o
-
--- cmd('syntax on')
--- vim.api.nvim_command('filetype plugin indent on')
-
-o.termguicolors = true
 vim.cmd [[ colorscheme peachpuff]]
--- o.background = 'dark'
 
--- Do not save when switching buffers
--- o.hidden = true
+local options = {
+  backup = false,                          -- creates a backup file
+  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  conceallevel = 0,                        -- so that `` is visible in markdown files
+  fileencoding = "utf-8",                  -- the encoding written to a file
+  hlsearch = true,                         -- highlight all matches on previous search pattern
+  ignorecase = true,                       -- ignore case in search patterns
+  mouse = "a",                             -- allow the mouse to be used in neovim
+  pumheight = 10,                          -- pop up menu height
+  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+  showtabline = 2,                         -- always show tabs
+  smartcase = true,                        -- smart case
+  smartindent = true,                      -- make indenting smarter again
+  splitbelow = true,                       -- force all horizontal splits to go below current window
+  splitright = true,                       -- force all vertical splits to go to the right of current window
+  swapfile = false,                        -- creates a swapfile
+  -- termguicolors = true,                    -- set term gui colors (most terminals support this)
+  timeoutlen = 300,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true,                         -- enable persistent undo
+  updatetime = 300,                        -- faster completion (4000ms default)
+  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true,                        -- convert tabs to spaces
+  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
+  tabstop = 2,                             -- insert 2 spaces for a tab
+  cursorline = true,                       -- highlight the current line
+  number = true,                           -- set numbered lines
+  relativenumber = false,                  -- set relative numbered lines
+  numberwidth = 4,                         -- set number column width to 2 {default 4}
+  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  wrap = false,                             -- display lines as one long line
+  linebreak = true,                        -- companion to wrap, don't split words
+  scrolloff = 8,                           -- minimal number of screen lines to keep above and below the cursor
+  sidescrolloff = 8,                       -- minimal number of screen columns either side of cursor if wrap is `false`
+  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
+}
 
--- Decrease update time
-o.timeoutlen = 500
-o.updatetime = 200
-
--- Number of screen lines to keep above and below the cursor
-o.scrolloff = 8
-
--- Better editor UI
-o.number = true
-o.numberwidth = 4
-o.cursorline = true
-
--- Better editing experience
-o.expandtab = true
--- o.smarttab = true
-o.cindent = true
--- o.autoindent = true
-o.wrap = false
-o.textwidth = 300
-o.tabstop = 4
-o.shiftwidth = 0
-o.softtabstop = -1 -- If negative, shiftwidth value is used
-o.list = true
-o.listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂'
--- o.listchars = 'eol:¬,space:·,lead: ,trail:·,nbsp:◇,tab:→-,extends:▸,precedes:◂,multispace:···⬝,leadmultispace:│   ,'
--- o.formatoptions = 'qrn1'
-
--- Makes neovim and host OS clipboard play nicely with each other
-o.clipboard = 'unnamedplus'
-
-    -- Case insensitive searching UNLESS /C or capital in search
-o.ignorecase = true
-o.smartcase = true
-
--- Undo and backup options
-o.backup = true
-o.writebackup = false
-o.undofile = true
-o.swapfile = false
--- o.backupdir = '/tmp/'
--- o.directory = '/tmp/'
--- o.undodir = '/tmp/'
-
--- Remember 50 items in commandline history
-o.history = 50
-
--- Better buffer splitting
-o.splitright = true
-o.splitbelow = true
-
--- Preserve view while jumping
-o.jumpoptions = 'view'
-
--- Stable buffer content on window open/close events.
-o.splitkeep = 'screen'
-
--- WARN: this won't update the search count after pressing `n` or `N`
--- When running macros and regexes on a large file, lazy redraw tells neovim/vim not to draw the screen
--- o.lazyredraw = true
-
--- Better folds (don't fold by default)
--- o.foldmethod = 'indent'
--- o.foldlevelstart = 99
--- o.foldnestmax = 3
--- o.foldminlines = 1
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
 -- Map <leader> to space
-g.mapleader = ' '
-g.maplocalleader = ' '
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
